ffa55b17964b8d17428515ee7974976d67efcaa7
Brad Larson <brad.larson@moblico.com>
2015-08-08 15:28:50 -0500

    Add support to register a new user

--- src/main/java/com/moblico/sdk/services/UsersService.java
+++ src/main/java/com/moblico/sdk/services/UsersService.java
@@ -26,4 +26,40 @@ public class UsersService {
             }
         });
     }
+
+    public static void registerUser(final User user, final Callback<Void> callback) {
+        AuthenticationService.authenticate(new ErrorForwardingCallback<Void>(callback) {
+            @Override
+            public void onSuccess(Void result) {
+                // Use reflection to convert all the fields in User to <K:V> pairs.  This way, when
+                // a field is added to user, we get it for free.  There might be easier ways to do
+                // this with existing libraries...
+                Map<String, String> params = new HashMap<String, String>();
+                for(Field field : User.class.getDeclaredFields()) {
+                    field.setAccessible(true);
+                    try {
+                        if (field.getType().equals(boolean.class)) {
+                            params.put(field.getName(), field.getBoolean(user) ? "YES" : "NO");
+                        } else if (field.getName().equals("attributes")) {
+                            // Skip attributes!
+                        } else if (java.lang.reflect.Modifier.isStatic(field.getModifiers())) {
+                            // Skip static fields!
+                        } else {
+                            Object value = field.get(user);
+                            if (value != null) {
+                                params.put(field.getName(), value.toString());
+                            }
+                        }
+                    } catch (IllegalAccessException e) {
+                    }
+                }
+                HttpRequest.post("users", params, new ErrorForwardingCallback<String>(callback) {
+                    @Override
+                    public void onSuccess(String result) {
+                        callback.onSuccess(null);
+                    }
+                });
+            }
+        });
+    }
 }
