030d11f08a7ed3ba39fa1edf54652739480f5698
Brad Larson <brad.larson@moblico.com>
2015-08-30 15:17:57 -0500

    Add API to check in to location

--- src/main/java/com/moblico/sdk/services/LocationsService.java
+++ src/main/java/com/moblico/sdk/services/LocationsService.java
@@ -4,7 +4,9 @@ import com.google.gson.reflect.TypeToken;
 import com.moblico.sdk.entities.Location;
 
 import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public final class LocationsService {
 
@@ -37,6 +39,22 @@ public final class LocationsService {
                     public void onSuccess(String result) {
                         Location location = Moblico.getGson().fromJson(result, Location.class);
                         callback.onSuccess(location);
+                    }
+                });
+            }
+        });
+    }
+
+    public static void checkin(final Location location, final Callback<Void> callback) {
+        AuthenticationService.authenticate(new ErrorForwardingCallback<Void>(callback) {
+            @Override
+            public void onSuccess(Void result) {
+                Map<String, String> params = new HashMap<>();
+                params.put("locationId", Long.toString(location.getId()));
+                HttpRequest.post("checkIn", params, new ErrorForwardingCallback<String>(callback) {
+                    @Override
+                    public void onSuccess(String result) {
+                        callback.onSuccess(null);
                     }
                 });
             }
