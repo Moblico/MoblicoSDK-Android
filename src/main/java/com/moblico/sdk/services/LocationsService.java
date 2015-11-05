139ec0da81a0551de18cbb56384d18f31a11f960
Brad Larson <brad.larson@moblico.com>
2015-11-04 23:48:57 -0600

    Pass in lat/lon for locations service

--- src/main/java/com/moblico/sdk/services/LocationsService.java
+++ src/main/java/com/moblico/sdk/services/LocationsService.java
@@ -1,5 +1,8 @@
 package com.moblico.sdk.services;
 
+import android.content.Context;
+import android.location.LocationManager;
+
 import com.google.gson.reflect.TypeToken;
 import com.moblico.sdk.entities.Location;
 
@@ -13,12 +16,23 @@ public final class LocationsService {
     private LocationsService() {
     }
 
-    public static void findLocations(final Callback<List<Location>> callback) {
+    /**
+     * Find locations.  To return a distance to the user, and sort the locations by distance,
+     * include the context parameter.  If distance isn't important, context can be null.
+     */
+    public static void findLocations(final Context context, final Callback<List<Location>> callback) {
         AuthenticationService.authenticate(new ErrorForwardingCallback<Void>(callback) {
             @Override
             public void onSuccess(Void result) {
-                // TODO: pass in current location, if known?
-                HttpRequest.get("locations", null, new ErrorForwardingCallback<String>(callback) {
+                Map<String, String> params = new HashMap<>();
+                if (context != null) {
+                    android.location.Location location = findLocation(context);
+                    if (location != null) {
+                        params.put("latitude", Double.toString(location.getLatitude()));
+                        params.put("longitude", Double.toString(location.getLongitude()));
+                    }
+                }
+                HttpRequest.get("locations", params, new ErrorForwardingCallback<String>(callback) {
                     @Override
                     public void onSuccess(String result) {
                         Type collectionType = new TypeToken<List<Location>>() {}.getType();
@@ -61,5 +75,21 @@ public final class LocationsService {
                 });
             }
         });
+    }
+
+    private static android.location.Location findLocation(Context context) {
+        LocationManager lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        android.location.Location gpsLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        android.location.Location networkLocation = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        if (gpsLocation == null) {
+            return networkLocation;
+        } else if (networkLocation == null) {
+            return gpsLocation;
+        }
+        if (gpsLocation.getTime() < networkLocation.getTime()) {
+            return networkLocation;
+        } else {
+            return gpsLocation;
+        }
     }
 }
