d2d1905f4554eb43f96a419b94ea873f2113d5e6
Brad Larson <brad.larson@moblico.com>
2016-07-18 20:23:47 -0500

    Pass in context with promo request

--- src/main/java/com/moblico/sdk/services/PromosService.java
+++ src/main/java/com/moblico/sdk/services/PromosService.java
@@ -1,21 +1,27 @@
 package com.moblico.sdk.services;
 
 
+import android.support.annotation.NonNull;
+
 import com.google.gson.reflect.TypeToken;
 import com.moblico.sdk.entities.Promos;
 
 import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PromosService {
     private PromosService() {
     }
 
-    public static void getPromos(final Callback<List<Promos>> callback){
+    public static void getPromos(@NonNull final String adContext, final Callback<List<Promos>> callback){
         AuthenticationService.authenticate(new ErrorForwardingCallback<Void>(callback) {
             @Override
             public void onSuccess(Void result) {
-                HttpRequest.get("promos", null, new ErrorForwardingCallback<String>(callback) {
+                Map<String, String> params = new HashMap<>();
+                params.put("context", adContext);
+                HttpRequest.get("promos", params, new ErrorForwardingCallback<String>(callback) {
                     @Override
                     public void onSuccess(String result) {
                         Type collectionType = new TypeToken<List<Promos>>() {}.getType();
