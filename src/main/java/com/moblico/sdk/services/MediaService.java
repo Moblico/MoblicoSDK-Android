b85e83d1f6f15e7ec653de4165c06999e24ff750
Brad Larson <brad.larson@moblico.com>
2017-03-12 22:06:26 -0500

    Allow finding media for a specific location
    Add support to look up media only for a specific location, by passing
    in a Location object to MediaService.findAllForLocation().

--- src/main/java/com/moblico/sdk/services/MediaService.java
+++ src/main/java/com/moblico/sdk/services/MediaService.java
@@ -1,6 +1,7 @@
 package com.moblico.sdk.services;
 
 import com.google.gson.reflect.TypeToken;
+import com.moblico.sdk.entities.Location;
 import com.moblico.sdk.entities.Media;
 
 import java.lang.reflect.Type;
@@ -23,6 +24,28 @@ public class MediaService {
             @Override
             public void onSuccess(Void result) {
                 HttpRequest.get("media", params, new ErrorForwardingCallback<String>(callback) {
+
+                    @Override
+                    public void onSuccess(String result) {
+                        Type collectionType = new TypeToken<List<Media>>() { }.getType();
+                        List<Media> media = Moblico.getGson().fromJson(result, collectionType);
+                        callback.onSuccess(media);
+                    }
+                });
+            }
+        } );
+    }
+
+    public static void findAllForLocation(final String category, final String mediaType, final String mediaTypeCategory, final Location location, final Callback<List<Media>> callback) {
+        final Map<String, String> params = new HashMap<String, String>();
+        if(category != null) params.put("category", category);
+        if(mediaType != null) params.put("mediaType", mediaType);
+        if(mediaTypeCategory != null) params.put("mediaTypeCategory", mediaTypeCategory);
+
+        AuthenticationService.authenticate(new ErrorForwardingCallback<Void>(callback) {
+            @Override
+            public void onSuccess(Void result) {
+                HttpRequest.get("locations/" + location.getId() + "/media", params, new ErrorForwardingCallback<String>(callback) {
 
                     @Override
                     public void onSuccess(String result) {
