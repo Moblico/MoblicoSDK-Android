963e4ded6c9926783e5cac1727a63b0e9092bb9a
Cameron Knight <cameron.knight@moblico.com>
2019-02-11 18:05:00 -0600

    Rename GcmService to DeviceService and remove GCM support.

--- src/main/java/com/moblico/sdk/services/DeviceService.java
+++ src/main/java/com/moblico/sdk/services/DeviceService.java
@@ -1,19 +1,14 @@
 package com.moblico.sdk.services;
 
 import android.content.Context;
-import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
-import com.moblico.sdk.entities.Status;
-import com.moblico.sdk.entities.User;
-import com.moblico.sdk.services.gcm.RegistrationService;
 
-import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
-public class GcmService {
+public class DeviceService {
 
     public static final String EXTRA_MESSAGE_ID = "com.moblico.private.intent.EXTRA_MESSAGE_ID";
     public static final String EXTRA_NOTIFICATION_TITLE = "com.moblico.intent.EXTRA_NOTIFICATION_TITLE";
@@ -32,7 +27,7 @@ public class GcmService {
         }
     }
 
-    private GcmService() {
+    private DeviceService() {
     }
 
     public static void receivedMessage(final long messageId, final @NonNull MessageStatus status, final Callback<Void> callback) {
@@ -68,22 +63,15 @@ public class GcmService {
         });
     }
 
-    /**
-     * Register the device with the user's account so they can receive push notifications from
-     * Moblico's server through GCM.
-     */
-    public static void registerDevice(final @NonNull Context context) {
-        Intent i = new Intent(context, RegistrationService.class);
-        context.startService(i);
-    }
+
 
     /**
-     * Unregister the device from GCM and Moblico so they will no longer receive push notifications.
+     * Unregister the device from Moblico so they will no longer receive push notifications.
      */
     public static void unregisterDevice(final @NonNull Context context) {
-        Intent i = new Intent(context, RegistrationService.class);
-        i.putExtra(RegistrationService.EXTRA_UNREGISTER, true);
-        context.startService(i);
+//        Intent i = new Intent(context, RegistrationService.class);
+//        i.putExtra(RegistrationService.EXTRA_UNREGISTER, true);
+//        context.startService(i);
         AuthenticationService.authenticate(new Callback<Void>() {
             @Override
             public void onSuccess(Void result) {
@@ -106,7 +94,7 @@ public class GcmService {
 
     /**
      * Register a user's device with the given device ID.  This is typically used as an internal
-     * API call, and most developers will want to call {@link GcmService#registerDevice(Context)}
+     * API call, and most developers will want to call {@link DeviceService# registerDevice(Context)}
      * instead.
      */
     public static void registerDevice(final @NonNull String deviceId, final Callback<Void> callback) {
