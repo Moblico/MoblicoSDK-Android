8b2dc2b997f2c65d1a078e31edb3bc55fd70ae3b
Cameron Knight <cameron.knight@moblico.com>
2019-03-22 11:44:06 -0500

    Add EventsService.getEvent to get a single event by id.

--- src/main/java/com/moblico/sdk/services/EventsService.java
+++ src/main/java/com/moblico/sdk/services/EventsService.java
@@ -3,17 +3,28 @@ package com.moblico.sdk.services;
 import com.google.gson.reflect.TypeToken;
 import com.moblico.sdk.entities.Event;
 import com.moblico.sdk.entities.Location;
-import com.moblico.sdk.entities.Reward;
 
 import java.lang.reflect.Type;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public final class EventsService {
 
     private EventsService() {
+    }
+
+    public static void getEvent(final long eventId, final Callback<Event> callback) {
+        AuthenticationService.authenticate(new ErrorForwardingCallback<Void>(callback) {
+            @Override
+            public void onSuccess(Void result) {
+                HttpRequest.get("events/" + eventId, null, new ErrorForwardingCallback<String>(callback) {
+                    @Override
+                    public void onSuccess(String result) {
+                        Event event = Moblico.getGson().fromJson(result, Event.class);
+                        callback.onSuccess(event);
+                    }
+                });
+            }
+        });
     }
 
     public static void getEvents(final Callback<List<Event>> callback) {
