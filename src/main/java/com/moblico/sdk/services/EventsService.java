60069c35d042cac1312134e723213107677debc3
Cameron Knight <cameron.knight@moblico.com>
2020-04-02 17:47:20 -0500

    Add support for retrieving events by type.

--- src/main/java/com/moblico/sdk/services/EventsService.java
+++ src/main/java/com/moblico/sdk/services/EventsService.java
@@ -5,6 +5,7 @@ import com.moblico.sdk.entities.Event;
 import com.moblico.sdk.entities.Location;
 
 import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.List;
 
 public final class EventsService {
@@ -28,10 +29,19 @@ public final class EventsService {
     }
 
     public static void getEvents(final Callback<List<Event>> callback) {
+        getEvents((String) null, callback);
+    }
+
+    public static void getEvents(final String type, final Callback<List<Event>> callback) {
         AuthenticationService.authenticate(new ErrorForwardingCallback<Void>(callback) {
             @Override
             public void onSuccess(Void result) {
-                HttpRequest.get("events", null, new ErrorForwardingCallback<String>(callback) {
+                HashMap<String, String> params = null;
+                if (type != null && type.length() > 0) {
+                    params = new HashMap<>();
+                    params.put("type", type);
+                }
+                HttpRequest.get("events", params, new ErrorForwardingCallback<String>(callback) {
                     @Override
                     public void onSuccess(String result) {
                         Type collectionType = new TypeToken<List<Event>>() {}.getType();
